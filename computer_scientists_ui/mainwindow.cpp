65ec72e698835683508bd0ebadb1c17052864fce
HAL2328 <halibkweli@gmail.com>
2015-12-15 21:02:51 +0000

    töflur read-only

--- computer_scientists_ui/mainwindow.cpp
+++ computer_scientists_ui/mainwindow.cpp
@@ -28,6 +28,8 @@ void MainWindow::display_scientists(vector<Scientist> scientists)
 {
     ui->table_scientists->setRowCount(scientists.size());
     ui->table_scientists->setSelectionBehavior(QAbstractItemView::SelectRows);
+    ui->table_scientists->setSelectionMode(QAbstractItemView::SingleSelection);
+    ui->table_scientists->setEditTriggers(QAbstractItemView::NoEditTriggers);
     for (unsigned int i = 0; i < scientists.size(); i++)
     {
         Scientist current = scientists[i];
@@ -52,6 +54,7 @@ void MainWindow::display_scientists(vector<Scientist> scientists)
     }
     ui->table_scientists->setColumnHidden(4, true);
     ui->table_scientists->resizeColumnsToContents();
+
     current_list_of_scientists = scientists;
 }
 
@@ -65,6 +68,8 @@ void MainWindow::display_computers(vector<Computers> computers)
 {
     ui->table_computers->setRowCount(computers.size());
     ui->table_computers->setSelectionBehavior(QAbstractItemView::SelectRows);
+    ui->table_computers->setSelectionMode(QAbstractItemView::SingleSelection);
+    ui->table_computers->setEditTriggers(QAbstractItemView::NoEditTriggers);
     for (unsigned int i = 0; i < computers.size(); i++)
     {
         Computers current = computers[i];
@@ -120,6 +125,8 @@ void MainWindow::display_connected_computers()
 void MainWindow::display_connected_scientists()
 {
     ui->table_connected_scientists->clear();
+    ui->table_connected_scientists->setSelectionMode(QAbstractItemView::SingleSelection);
+    ui->table_connected_scientists->setEditTriggers(QAbstractItemView::NoEditTriggers);
     vector<Scientist> connected_scientists = computer_service.joined_scientists(current_computer_id);
     ui->table_connected_scientists->setRowCount(connected_scientists.size());
     ui->table_connected_scientists->setColumnCount(2);
